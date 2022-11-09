d86eec0f69f0d34138a03759c8b90f801e097949
mrtclgst <mrtclgst@gmail.com>
2022-11-09 18:38:27 +0300

    .

--- Assets/_Scripts/SceneManagement/Fader.cs
+++ Assets/_Scripts/SceneManagement/Fader.cs
@@ -6,32 +6,57 @@ namespace RPG.SceneManagement
 {
     public class Fader : MonoBehaviour
     {
-        CanvasGroup fader;
+        CanvasGroup m_Fader;
+        private Coroutine m_CurrentActiveFade = null;
 
         private void Awake()
         {
-            fader = GetComponent<CanvasGroup>();
+            m_Fader = GetComponent<CanvasGroup>();
         }
+
         public void FadeOutImmediate()
         {
-            fader.alpha = 1;
+            m_Fader.alpha = 1;
         }
 
         public IEnumerator FadeOut(float time)
         {
-            while (fader.alpha < 1)
+            if (m_CurrentActiveFade != null)
             {
-                fader.alpha += Time.deltaTime / time; //sahne gecisi arasinin alpha degeri
-                yield return null;
+                StopCoroutine(m_CurrentActiveFade);
             }
+
+            m_CurrentActiveFade = StartCoroutine(FadeOutRoutine(time));
+            yield return m_CurrentActiveFade;
         }
+
         public IEnumerator FadeIn(float time)
         {
-            while (fader.alpha > 0)
+            if (m_CurrentActiveFade != null)
             {
-                fader.alpha -= Time.deltaTime / time; //sahne gecisi arasinin alpha degeri
+                StopCoroutine(m_CurrentActiveFade);
+            }
+
+            m_CurrentActiveFade = StartCoroutine(FadeInRoutine(time));
+            yield return m_CurrentActiveFade;
+        }
+
+        private IEnumerator FadeRoutine(float target, float time)
+        {
+            while (m_Fader.alpha > 0)
+            {
+                m_Fader.alpha -= Time.deltaTime / time; //sahne gecisi arasinin alpha degeri
                 yield return null;
             }
         }
+        
+        // private IEnumerator FadeOutRoutine(float time)
+        // {
+        //     while (m_Fader.alpha < 1)
+        //     {
+        //         m_Fader.alpha += Time.deltaTime / time; //sahne gecisi arasinin alpha degeri
+        //         yield return null;
+        //     }
+        // }
     }
 }
